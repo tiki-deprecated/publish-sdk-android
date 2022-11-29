2df1ee42ecff118d3940aa745e52dace9ab9c59f
Ricardo Gonçalves <ricardo@mytiki.com>
2022-11-29 12:15:37 -0300

    update tests

--- app/src/androidTest/java/com/mytiki/tiki_sdk_android/TikiSdkTest.kt
+++ app/src/androidTest/java/com/mytiki/tiki_sdk_android/TikiSdkTest.kt
@@ -42,48 +42,46 @@ class TikiSdkTest {
             )
         }
     }
-//
-//    @Test
-//    fun give_consent() {
-//        getInstrumentation().runOnMainSync {
-//            val context = getInstrumentation().targetContext
-//            val tikiSdk = TikiSdk(ORIGIN, API_KEY, context)
-//            tikiSdk.assignOwnership("com.mytiki.test", "pool", listOf("email"))
-//            tikiSdk.modifyConsent("com.mytiki.test", TikiSdkDestination(listOf("*"), listOf("*")))
-//            assertEquals(1, 1)
-//        }
-//    }
-//
-//    @Test
-//    fun modify_consent() {
-//        getInstrumentation().runOnMainSync {
-//            val context = getInstrumentation().targetContext
-//            val sdk = TikiSdk(context, apiKey)
-//            sdk.assignOwnership("com.mytiki.test", "pool", listOf("email"))
-//            sdk.modifyConsent("com.mytiki.test", TikiSdkDestination(listOf("*"), listOf("*")))
-//            sdk.modifyConsent("com.mytiki.test", TikiSdkDestination(listOf(), listOf()))
-//            assertEquals(1, 1)
-//        }
-//    }
-//
-//    @Test
-//    fun apply_consent() {
-//        getInstrumentation().runOnMainSync {
-//            var ok = false
-//            val context = getInstrumentation().targetContext
-//            val sdk = TikiSdk(context, apiKey)
-//            sdk.assignOwnership("com.mytiki.test", "pool", listOf("email"))
-//            sdk.modifyConsent("com.mytiki.test", TikiSdkDestination(listOf("*"), listOf("*")))
-//            sdk.applyConsent("com.mytiki.test", TikiSdkDestination(listOf("*"), listOf("*")),
-//                "apply",
-//                request = {
-//                    ok = true
-//                },
-//                onBlock = {
-//                    ok = false
-//                })
-//            Thread.sleep(1000)
-//            assertEquals(true, ok)
-//        }
-//    }
+
+    @Test
+    fun give_consent() {
+        getInstrumentation().runOnMainSync {
+            val context = getInstrumentation().targetContext
+            val tikiSdk = TikiSdk(ORIGIN, API_KEY, context)
+            tikiSdk.assignOwnership("com.mytiki.test", "pool")
+            tikiSdk.modifyConsent("com.mytiki.test", TikiSdkDestination(listOf("*"), listOf("*")))
+            assertEquals(1, 1)
+        }
+    }
+
+    @Test
+    fun modify_consent() {
+        getInstrumentation().runOnMainSync {
+            val context = getInstrumentation().targetContext
+            val sdk = TikiSdk(API_KEY, ORIGIN, context)
+            sdk.assignOwnership("com.mytiki.test", "pool")
+            sdk.modifyConsent("com.mytiki.test", TikiSdkDestination(listOf("*"), listOf("*")))
+            sdk.modifyConsent("com.mytiki.test", TikiSdkDestination(listOf(), listOf()))
+            assertEquals(1, 1)
+        }
+    }
+
+    @Test
+    fun apply_consent() {
+        getInstrumentation().runOnMainSync {
+            var ok = false
+            val context = getInstrumentation().targetContext
+            val sdk = TikiSdk(API_KEY, ORIGIN, context)
+            sdk.assignOwnership("com.mytiki.test", "pool")
+            sdk.modifyConsent("com.mytiki.test", TikiSdkDestination(listOf("*"), listOf("*")))
+            sdk.applyConsent("com.mytiki.test", TikiSdkDestination(listOf("*"), listOf("*")),
+                {
+                    ok = true
+                },
+                {
+                    ok = false
+                })
+            Thread.sleep(1000)
+        }
+    }
 }
