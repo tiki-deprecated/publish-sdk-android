1f012de5b26b24beab2d0c892966e78bf515e3ec
Ricardo Gonçalves <ricardo@mytiki.com>
2022-12-21 11:23:09 -0300

    update sdk to 0.0.23 update flutterEngine remove coroutines from tests - tests are failing

--- app/src/androidTest/java/com/mytiki/tiki_sdk_android/TikiSdkTest.kt
+++ app/src/androidTest/java/com/mytiki/tiki_sdk_android/TikiSdkTest.kt
@@ -2,13 +2,8 @@ package com.mytiki.tiki_sdk_android
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
-import kotlinx.coroutines.*
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.setMain
-import org.junit.After
 import org.junit.Assert
 import org.junit.Assert.assertEquals
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
