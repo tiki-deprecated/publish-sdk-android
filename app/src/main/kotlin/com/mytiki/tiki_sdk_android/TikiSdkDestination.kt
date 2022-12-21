d1673055a821a076a3e8e9cc280c6db6b49b51a1
Ricardo Gonçalves <ricardo@mytiki.com>
2022-12-21 12:28:38 -0300

    TikiSdkDestination with tests

--- app/src/main/kotlin/com/mytiki/tiki_sdk_android/TikiSdkDestination.kt
+++ app/src/main/kotlin/com/mytiki/tiki_sdk_android/TikiSdkDestination.kt
@@ -1,28 +1,12 @@
 package com.mytiki.tiki_sdk_android
 
-import org.json.JSONArray
-import org.json.JSONObject
+import com.squareup.moshi.JsonClass
 
-data class TikiSdkDestination(val uses: List<String>, val paths: List<String>) {
-    companion object {
-        fun fromJson(jsonString: String): TikiSdkDestination {
-            val jsonObject = JSONObject(jsonString)
-            val usesArr: JSONArray = jsonObject.getJSONArray("uses")
-            val uses: MutableList<String> =
-                MutableList(usesArr.length()) { i -> usesArr.getString(i) }
-            val pathsArr = jsonObject.getJSONArray("paths")
-            val paths = MutableList<String>(pathsArr.length()) { i -> pathsArr.getString(i) }
-            return TikiSdkDestination(
-                uses,
-                paths
-            )
-        }
-    }
-
-    fun toJson(): String {
-        val jsonObject = JSONObject()
-        jsonObject.put("uses", JSONArray(uses))
-        jsonObject.put("paths", JSONArray(paths))
-        return jsonObject.toString()
-    }
+@JsonClass(generateAdapter = true)
+open class TikiSdkDestination(
+    val paths: List<String>,
+    val uses: List<String> = listOf("*"),
+){
+    object ALL : TikiSdkDestination(listOf("*"))
+    object NONE : TikiSdkDestination(listOf(), listOf())
 }
