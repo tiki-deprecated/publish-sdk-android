9aed551c1765535dbee45ac68c356482bddc51c8
Ricardo Gonçalves <ricardo@mytiki.com>
2022-12-21 18:42:26 -0300

    ReqConsentApply docs and tests

--- app/src/main/kotlin/com/mytiki/tiki_sdk_android/tiki_platform_channel/req/ReqConsentApply.kt
+++ app/src/main/kotlin/com/mytiki/tiki_sdk_android/tiki_platform_channel/req/ReqConsentApply.kt
@@ -23,5 +23,5 @@ data class ReqConsentApply(
     override val requestId: String,
     val source: String,
     val destination: TikiSdkDestination,
-    val origin: String?
+    val origin: String? = null
 ) : Req(requestId)
---
+++ app/src/test/java/com/mytiki/tiki_sdk_android/tiki_platform_channel/req/ReqConsentApplyTest.kt
@@ -0,0 +1,28 @@
+package com.mytiki.tiki_sdk_android
+
+import com.mytiki.tiki_sdk_android.tiki_platform_channel.req.ReqConsentApply
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import org.junit.Assert
+import org.junit.Test
+
+class ReqConsentApplyTest {
+    @Test
+    fun encode_ReqConsentApply_from_JSON() {
+        val moshi: Moshi = Moshi.Builder().build()
+        val adapter: JsonAdapter<ReqConsentApply> = moshi.adapter(ReqConsentApply::class.java)
+        val json = "{\"requestId\":\"reqId\",\"source\":\"source\",\"destination\":{\"paths\":[\"*\"],\"uses\":[\"*\"]}}"
+        val req = adapter.fromJson(json)
+        Assert.assertEquals(req!!.destination.paths[0], "*")
+        Assert.assertEquals(req.requestId, "reqId")
+        Assert.assertEquals(req.source, "source")
+    }
+    @Test
+    fun encode_ReqConsentApply_to_JSON() {
+        val moshi: Moshi = Moshi.Builder().build()
+        val adapter: JsonAdapter<ReqConsentApply> = moshi.adapter(ReqConsentApply::class.java)
+        val req = ReqConsentApply("reqId", "source", TikiSdkDestination.ALL)
+        val json = adapter.toJson(req)
+        Assert.assertEquals("{\"requestId\":\"reqId\",\"source\":\"source\",\"destination\":{\"paths\":[\"*\"],\"uses\":[\"*\"]}}",json)
+    }
+}
