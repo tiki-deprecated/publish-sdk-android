bc0e6d9091c7aa53bd9e447a2679175c10f8d60e
Ricardo Gonçalves <ricardo@mytiki.com>
2022-12-26 08:48:19 -0300

    add TimeStampToDateAdapter update TikiSdkConsent

--- app/src/test/java/com/mytiki/tiki_sdk_android/TikiSdkConsentTest.kt
+++ app/src/test/java/com/mytiki/tiki_sdk_android/TikiSdkConsentTest.kt
@@ -1,35 +1,42 @@
 package com.mytiki.tiki_sdk_android
 
+import com.mytiki.tiki_sdk_android.util.TimeStampToDateAdapter
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import org.junit.Assert
 import org.junit.Test
+import java.util.*
 
 class TikiSdkConsentTest {
     @Test
     fun encode_TikiSdkConsent_from_JSON() {
-        val moshi: Moshi = Moshi.Builder().build()
+        val moshi: Moshi = Moshi.Builder()
+            .add(TimeStampToDateAdapter())
+            .build()
         val adapter: JsonAdapter<TikiSdkConsent> = moshi.adapter(TikiSdkConsent::class.java)
         val tikiSdkConsent =
-            adapter.fromJson("{\"ownershipId\":\"ownership\",\"destination\":{\"paths\":[\"*\"],\"uses\":[\"*\"]},\"transactionId\":\"txnId\"}")
+            adapter.fromJson("{\"ownershipId\":\"ownership\",\"destination\":{\"paths\":[\"*\"],\"uses\":[\"*\"]},\"transactionId\":\"txnId\",\"expiry\":1671842900000}")
         Assert.assertEquals("ownership", tikiSdkConsent?.ownershipId)
         Assert.assertEquals("*", tikiSdkConsent?.destination?.uses?.get(0))
         Assert.assertEquals("txnId", tikiSdkConsent?.transactionId)
+        Assert.assertEquals(1671842900000L, tikiSdkConsent?.expiry?.time)
         Assert.assertNull(tikiSdkConsent?.about)
         Assert.assertNull(tikiSdkConsent?.reward)
-        Assert.assertNull(tikiSdkConsent?.expiry)
     }
 
     @Test
     fun encode_TikiSdkConsent_to_JSON() {
-        val moshi: Moshi = Moshi.Builder().build()
+        val moshi: Moshi = Moshi.Builder()
+            .add(TimeStampToDateAdapter())
+            .build()
         val adapter: JsonAdapter<TikiSdkConsent> = moshi.adapter(TikiSdkConsent::class.java)
         val tikiSdkConsent = TikiSdkConsent(
-            "ownership", TikiSdkDestination.ALL, "txnId"
+            "ownership", TikiSdkDestination.ALL, "txnId", null, null, Date(1671842900000L)
         )
+
         val json = adapter.toJson(tikiSdkConsent)
         Assert.assertEquals(
-            "{\"ownershipId\":\"ownership\",\"destination\":{\"paths\":[\"*\"],\"uses\":[\"*\"]},\"transactionId\":\"txnId\"}",
+            "{\"ownershipId\":\"ownership\",\"destination\":{\"paths\":[\"*\"],\"uses\":[\"*\"]},\"transactionId\":\"txnId\",\"expiry\":1671842900000}",
             json
         )
     }
