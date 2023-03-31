f6f18382e86cea4e8242e26205ae380e1f069961
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-03-31 11:29:54 -0300

    fix: remove unused resources

--- app/src/main/kotlin/com/mytiki/tiki_sdk_android/ui/screens/LearnMore.kt
+++ app/src/main/kotlin/com/mytiki/tiki_sdk_android/ui/screens/LearnMore.kt
@@ -4,15 +4,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.mytiki.tiki_sdk_android.R
 import com.mytiki.tiki_sdk_android.TikiSdk
 
 @Composable
 fun LearnMore() {
-    val learnMoreText = stringResource(id = R.string.learn_more)
+    val learnMoreText =
+        LocalContext.current.assets.open("learnMore.md").bufferedReader().use { it.readText() }
     val context = LocalContext.current
     LazyColumn {
         item {
