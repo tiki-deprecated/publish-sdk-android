e169a920870c825196e53d7ffe221c22fd9f60b8
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-04-03 18:37:42 -0300

    fix: clean example app

--- example_app/app/src/main/java/com/mytiki/tiki_sdk_android/example_app/MainActivity.kt
+++ example_app/app/src/main/java/com/mytiki/tiki_sdk_android/example_app/MainActivity.kt
@@ -1,29 +1,11 @@
 package com.mytiki.tiki_sdk_android.example_app
 
-import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import com.mytiki.tiki_sdk_android.example_app.databinding.MainActivityBinding
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: MainActivityBinding
-
-    private val viewModel by viewModels<HomeViewModel>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = MainActivityBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        setContentView(R.layout.activity_main)
     }
 }
