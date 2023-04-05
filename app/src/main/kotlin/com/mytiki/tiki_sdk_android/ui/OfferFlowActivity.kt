ca87caeaf1e73672b25c5ae813ffc84725863ac2
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-04-05 07:12:16 -0300

    fix: remove compose, work with xml

---
+++ app/src/main/kotlin/com/mytiki/tiki_sdk_android/ui/OfferFlowActivity.kt
@@ -0,0 +1,12 @@
+package com.mytiki.tiki_sdk_android.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mytiki.tiki_sdk_android.R
+
+class OfferFlowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_offer_flow)
+    }
+}
